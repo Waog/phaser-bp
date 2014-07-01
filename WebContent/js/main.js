555ebbfeafb780137ad5b8c3d0c2ab5c8004494f
oli <oliver.stadie@gmail.com>
2014-07-01 18:19:34 +0200

    FIXED: removed the original phaser logo
    ... replaced it by a reference to a placeholder image.

--- WebContent/js/main.js
+++ WebContent/js/main.js
@@ -1,13 +1,19 @@
+// ########################################################
+// #### replace this code for example by code          ####
+// #### from http://gamemechanicexplorer.com/          ####
+// #### Dont forget to replace the asset pathes.       ####
+// ########################################################
+
 var game = new Phaser.Game(800, 600, Phaser.AUTO, '', {
 	preload : preload,
 	create : create
 });
 
 function preload() {
-	game.load.image('logo', 'img/phaser.png');
+	game.load.image('star', 'assets/placeholder/img/starBlue.png');
 }
 
 function create() {
-	var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'logo');
+	var logo = game.add.sprite(game.world.centerX, game.world.centerY, 'star');
 	logo.anchor.setTo(0.5, 0.5);
 }
